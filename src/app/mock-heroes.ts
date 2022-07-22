c815efd1c00f156d6ab3b2e21b2a34f8feb29cac
Logan Murray <lpmurray16@users.noreply.github.com>
2022-07-22 11:50:25 -0500

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,38 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {
+    id: 2,
+    name: 'Jack-o-Lantern',
+    description:
+      'Bitten by a radioactive pumpkin, they are a Class C level hero.',
+    powers: ['Summon Bats', 'Shadow Jumping', 'Summon Light'],
+  },
+  {
+    id: 3,
+    name: 'Bombasto',
+    description: 'Bitten by a radioactive bomb, they are a Class B level hero.',
+    powers: ['Throw Bombs', 'Self Explosion', 'Immune to Bombs'],
+  },
+  {
+    id: 4,
+    name: 'Spoon Man',
+    description:
+      'Bitten by a radioactive spoon, they are a Class D level hero.',
+    powers: ['Flight (On Giant Spoon)', 'Heightened Reflexes', 'Scoops Stuff'],
+  },
+  {
+    id: 5,
+    name: 'Z Blade',
+    description:
+      'Bitten by a radioactive sword, they are a Class B level hero.',
+    powers: ['Super Jump', 'Heightened Reflexes', 'Throws Knifes'],
+  },
+  {
+    id: 6,
+    name: 'RubberMan',
+    description:
+      'Bitten by a radioactive piece of rubber, they are a Class B level hero.',
+    powers: ['Super Stretch', 'Bullet Proof', 'Super Intelligent'],
+  },
+];
