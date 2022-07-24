8cca5619f7594ece0d6622e7397b2c8c19c3eaf0
Logan Murray <lpmurray16@users.noreply.github.com>
2022-07-24 17:00:57 -0500

    on part 5 about routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,14 +12,17 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
-
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
   ngOnInit(): void {
     this.getHeroes();
   }
-  constructor(private heroService: HeroService) {}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
